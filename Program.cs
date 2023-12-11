7d61a5a43bdb6291c4a5b8f2b5282e558654bb9b
Gustavo Henrique <Gustavoproject01@gmail.com>
2023-12-11 14:55:59 -0300

    Desafio do Bootcamp - Commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using projeto_dio.Models;
+
+Console.WriteLine("Nokia :");
+Smartphone nokia = new Nokia(numero: "8199554477" , modelo: "Modelo 1", imei: "11111111", memoria: 64);
+nokia.Apresentar();
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+
+Console.WriteLine("\n");
+
+
+Console.WriteLine("Iphone :");
+Smartphone iphone = new Iphone(numero: "8199884455" , modelo: "Modelo 2", imei: "2222111", memoria: 124);
+iphone.Apresentar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Linkedin");
